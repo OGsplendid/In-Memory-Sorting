06c6afcac553182ff237922cde11d4e7e7d68887
Vladimir Sivakov <vladimir.a.sivakov@gmail.com>
2023-08-01 16:42:12 +0300

    code in app.js fixed

--- src/js/app.js
+++ src/js/app.js
@@ -39,13 +39,14 @@ const titles = table.querySelectorAll('.cell');
 
 for (const title of titles) {
   title.onclick = () => {
+    const name = title.textContent;
     if (title.classList.contains('sorted')) {
       title.classList.remove('sorted');
-      data.sort((a, b) => b - a);
+      data.sort((a, b) => b[name] - a[name]);
       table.renderTable(data);
       return;
     }
-    data.sort((a, b) => a - b);
+    data.sort((a, b) => a[name] - b[name]);
     title.classList.add('sorted');
     table.renderTable(data);
   };
