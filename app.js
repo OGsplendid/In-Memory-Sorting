0c4b6bc8d79b5ba4b8fafc07fdf9784fadaa7217
Vladimir Sivakov <vladimir.a.sivakov@gmail.com>
2023-08-10 21:52:58 +0300

    redone eventListener

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
 import Table from './src/js/Table';
 import './src/css/style.css';
+import data from './src/js/dataBase.json';
 
-const data = [
-  {
-    id: 26,
-    title: 'Побег из Шоушенка',
-    imdb: 9.30,
-    year: 1994,
-  },
-  {
-    id: 25,
-    title: 'Крёстный отец',
-    imdb: 9.20,
-    year: 1972,
-  },
-  {
-    id: 27,
-    title: 'Крёстный отец 2',
-    imdb: 9.00,
-    year: 1974,
-  },
-  {
-    id: 1047,
-    title: 'Тёмный рыцарь',
-    imdb: 9.00,
-    year: 2008,
-  },
-  {
-    id: 223,
-    title: 'Криминальное чтиво',
-    imdb: 8.90,
-    year: 1994,
-  },
-];
-
-const table = new Table(document.querySelector('.table'));
-Table.renderTable(data);
-table.addEvents(data);
+const table = new Table(document.querySelector('.table'), data);
+table.renderTable();
--- src/js/Table.js
+++ src/js/Table.js
@@ -1,45 +1,55 @@
 export default class Table {
-  constructor(element) {
+  constructor(element, array) {
     try {
       this.element = element;
     } catch (error) {
-      console.log(error);
+      throw new Error('error');
     }
+
+    this.array = array;
+
+    this.onClick = this.onClick.bind(this);
+    this.element.addEventListener('click', this.onClick);
   }
 
   static createRow(obj) {
-    const row = document.createElement('tr');
-    const item = document.createElement('td');
+    const row = document.createElement('ul');
+    row.classList.add('row', 'sub');
     for (const key in obj) {
-      item.classList.add(key, 'cell');
+      const item = document.createElement('li');
+      item.classList.add('cell');
       item.textContent = obj[key];
       row.insertAdjacentElement('beforeend', item);
     }
     return row;
   }
 
-  renderTable(array) {
-    for (const item of array) {
+  renderTable() {
+    for (const item of this.array) {
       const movie = Table.createRow(item);
       this.element.insertAdjacentElement('beforeend', movie);
     }
   }
 
-  addEvents(data) {
-    const titles = this.element.querySelectorAll('.main-cell');
-    for (const title of titles) {
-      title.onclick = () => {
-        const name = title.textContent;
-        if (title.classList.contains('sorted')) {
-          title.classList.remove('sorted');
-          data.sort((a, b) => b[name] - a[name]);
-          this.renderTable(data);
-          return;
-        }
-        data.sort((a, b) => a[name] - b[name]);
-        title.classList.add('sorted');
-        this.renderTable(data);
-      };
+  clearTable() {
+    const rows = this.element.querySelectorAll('.sub');
+    rows.forEach((el) => {
+      el.remove();
+    });
+  }
+
+  onClick(e) {
+    if (e.target.classList.contains('main-cell')) {
+      const key = e.target.textContent;
+      if (this.element.classList.contains('sorted')) {
+        this.array.sort((a, b) => b[key] - a[key]);
+        this.element.classList.remove('sorted');
+      } else {
+        this.array.sort((a, b) => a[key] - b[key]);
+        this.element.classList.add('sorted');
+      }
+      this.clearTable();
+      this.renderTable();
     }
   }
 }
